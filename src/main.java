be26ba71b0b24faf462a6853072c2adad7c03edb
Eymeric22 <97096493+Eymeric22@users.noreply.github.com>
2022-12-15 21:00:58 +0100

    Le programme tourne tant qu'on entre pas 0.

--- src/main.java
+++ src/main.java
@@ -9,10 +9,16 @@ public class main {
     public static void main(String[] args) throws FileNotFoundException {
         JFileChooser select = new JFileChooser("D:\\IntelliJ\\Workspace\\LothProject");
         select.showOpenDialog(null);
+        File f = select.getSelectedFile();
         Scanner sc = new Scanner(System.in);
         System.out.print("Entrez le nombre de mots voulus dans le proverbe : ");
         int nbMots = sc.nextInt();
-        File f = select.getSelectedFile();
-        System.out.print("Voici le proverbe : " + FunctionFile.randomize(FunctionFile.readFile(f), nbMots));
+        System.out.println("Voici le proverbe : " + FunctionFile.randomize(FunctionFile.readFile(f), nbMots));
+        while(nbMots != 0) {
+            System.out.println("Entrez le nombre de mots voulus dans le proverbe : ");
+            nbMots = sc.nextInt();
+            System.out.println("Voici le proverbe : " + FunctionFile.randomize(FunctionFile.readFile(f), nbMots));
+        }
+            System.out.println("Fin du programme.");
     }
 }
